38e5d7cc81e067ca53f47af2c62c264ff4360ce2
R0ll1ngSt0ne <michael.kostukov@gmail.com>
2022-01-05 17:53:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const run = require('./src');
+const core = require('@actions/core');
 
-run();
+const validatePrTitleOrSingleCommit = require('./src/validatePrTitleOrSingleCommit');
+const validateCommitMessages = require('./src/validateCommitMessages');
+
+let validationErrorPrTitleOrSingleCommit;
+let validationErrorCommitMessages;
+
+try {
+    validatePrTitleOrSingleCommit();
+} catch (error) {
+    validationErrorPrTitleOrSingleCommit = error;
+}
+
+try {
+    validateCommitMessages();
+} catch (error) {
+    validationErrorCommitMessages = error;
+}
+
+if (validationErrorPrTitleOrSingleCommit && validationErrorCommitMessages) {
+    core.setFailed(validationErrorPrTitleOrSingleCommit.message + " / " + validationErrorCommitMessages.message);
+}
