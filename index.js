2660e4618b09548928f2172208bd6353f7657881
R0ll1ngSt0ne <michael.kostukov@gmail.com>
2022-01-05 23:05:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const core = require('@actions/core');
-
-const validatePrTitleOrSingleCommit = require('./src/validatePrTitleOrSingleCommit');
 const validateCommitMessages = require('./src/validateCommitMessages');
+const validatePrTitleOrSingleCommit = require('./src/validatePrTitleOrSingleCommit');
 
 let validationErrorPrTitleOrSingleCommit;
 let validationErrorCommitMessages;
 
-try {
-    await validatePrTitleOrSingleCommit()
-} catch (e) {
-    validationErrorPrTitleOrSingleCommit = error;
+async function run() {
+  try {
+    await validatePrTitleOrSingleCommit();
+  } catch (e) {
+    validationErrorPrTitleOrSingleCommit = e;
+  }
+
+  try {
+    await validateCommitMessages();
+  } catch (e) {
+    validationErrorCommitMessages = e;
+  }
+
+  if (validationErrorPrTitleOrSingleCommit && validationErrorCommitMessages) {
+    core.setFailed(
+      validationErrorPrTitleOrSingleCommit.message +
+        ' / ' +
+        validationErrorCommitMessages.message
+    );
+  }
 }
 
-try {
-    await validateCommitMessages()
-} catch (e) {
-    validationErrorCommitMessages = error;
-}
-
-if (validationErrorPrTitleOrSingleCommit && validationErrorCommitMessages) {
-    core.setFailed(validationErrorPrTitleOrSingleCommit.message + " / " + validationErrorCommitMessages.message);
-}
+run();
